7d095986dbdf79bc8fbd3750681bfdcfd1db3f08
ABHAY PRAVIN YADAV <abhayvpn@gmail.com>
2022-12-29 12:38:11 +0530

    Updated App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import Header from './Documents/firstdisplay';
 import Visdir from './Documents/visdirect';
+import Ongoingcontests from './Documents/ongoing';
 import Upcontest from './Documents/upcontest';
-import Allcontests from './Documents/allcontests';
 import Footer from './Documents/footer'
 function App() {
   return (
     <>
       <Header />
       <Visdir />
+      <Ongoingcontests />
       <Upcontest />
-      <Allcontests />
       <Footer />
     </>
   );
